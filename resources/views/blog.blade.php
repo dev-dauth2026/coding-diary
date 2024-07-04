5458c10b6477e86720b858b6e5e6840e8fa22ac4
Dauth Gurung <dauth22grg22@gmail.com>
2024-07-05 08:17:54 +1000

    making image responsive

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -22,14 +22,14 @@
                             <small class="text-secondary">Published on:{{ $latestPost->created_at->format('M d, Y') }} </small>
                         </div>
                         <div class="d-flex justify-content-center align-items-center" >
-                            <img src="{{ asset('storage/' . $latestPost->image) }}" alt="{{ $latestPost->title }}" class="img-fluid" style="max-height: 600px;">
+                            <img src="{{ asset('storage/' . $latestPost->image) }}" alt="{{ $latestPost->title }}" class="img-fluid" style="width: 600px; min-width:200px; height:auto;">
                         </div>
                                                 
                         <p>{!! $latestPost->content !!} </p>
                     
                     @endif
                 </div>
-                <div class="list-group col-12 col-sm-12 col-md-3 col-lg-3">
+                <div class="list-group col-12 col-sm-12 col-md-3 col-lg-2 ">
                     <form action="" class="mb-5 d-sm-none">
                         @csrf
                         <div class="form-group d-flex gap-2">
@@ -40,13 +40,12 @@
                     </form>
                     <h4 class="">Recent Blogs</h4>
                     @foreach ($allBlogs as $post)
-
                        
-                            <div class="d-flex w-100 flex-column">
-                               <a href="#"><h5 class="mb-1">{{ $post->title }}</h5></a> 
-                                <small>{{ $post->created_at->format('M d, Y') }}</small>
-                                <small>By John</small>
-                            </div>
+                        <div class="d-flex  flex-column">
+                            <a href="#"><h6 class="mb-1">{{ $post->title }}</h6></a> 
+                            <small>{{ $post->created_at->format('M d, Y') }}</small>
+                            <small>By John</small>
+                        </div>
                  
                     @endforeach
                 </div>
