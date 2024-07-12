9f8b59062bc979f86abf6e909ce14555b706f23d
Dauth Gurung <dauth22grg22@gmail.com>
2024-07-13 03:53:27 +1000

    PostController updated

---
+++ app/Http/Controllers/admin/PostController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Validator;
+
+class PostController extends Controller
+{
+    public function createBlog(){
+        return view('admin.createBlog');
+    }
+
+    public function processCreateBlog(Request $request){
+        $validator = Validator::make($request->all(),[
+            'title' =>'required|min:3',
+            'content' =>'required',
+            'tags' =>'required',
+            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
+        ]);
+
+        if ($validator->fails()) {
+            // Store temporary image path in session if an image was uploaded
+            if ($request->hasFile('image')) {
+                $tempImagePath = $request->file('image')->store('images/temp', 'public');
+                Session::put('temp_image', $tempImagePath);
+            }
+
+            // Redirect back to the form with errors and old input
+            return redirect()->route('admin.createBlog')->withErrors($validator)->withInput();
+        }
+
+        if($validator->passes()){
+            $post = new Post();
+            $post->title = $request->title;
+            $post->content = $request->content;
+            $post->tags = $request->tags;
+
+            if ($request->hasFile('image')) {
+                $image = $request->file('image');
+                $imagePath = $image->store('images', 'public');
+                $post->image = $imagePath;
+                session()->forget('temp_image');
+            }elseif (session('temp_image')) {
+                $post->image = session('temp_image');
+                session()->forget('temp_image');
+            }
+
+            $post->save();
+
+            return redirect()->route('admin.dashboard')->with('postCreated','The post has been successfully created!');
+        }
+
+
+    }
+}
