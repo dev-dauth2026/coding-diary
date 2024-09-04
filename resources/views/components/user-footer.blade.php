cc8f3147899293ec18574fdb38ce24d0b7e07409
Dauth Gurung <dauth22grg22@gmail.com>
2024-09-04 21:33:55 +1000

    user-footer component created

---
+++ resources/views/components/user-footer.blade.php
@@ -0,0 +1,49 @@
+<footer class=" footer-section position-relative   text-white  w-100" >
+    <div class="container-fluid m-0 p-0 ">
+        <div class="p-5 bg-dark " style="--bs-bg-opacity: .85; ">
+            <div class="row " >
+                <div class="col-md-4">
+                    <h4>Know more about us</h4>
+                    <p class="text-white text-opacity-50">Learn coding more effectively from Coding Diary blog.</p>
+                </div>
+                <div class="col-md-4">
+                    <h4>Quick Links</h4>
+                    <ul class="list-unstyled">
+                        <li><a href="/" class="text-white text-opacity-50">Home</a></li>
+                        <li><a href="{{route('account.blog')}}" class="text-white text-opacity-50">Blogs</a></li>
+                        <li><a href="{{route('account.about')}}" class="text-white text-opacity-50">About</a></li>
+                        <li><a href="{{route('account.contact')}}" class="text-white text-opacity-50">Contact</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-4">
+                    <h4>Contact Us</h4>
+                    <p class="text-white text-opacity-50">Email: codingdiary2020@gmail.com</p>
+                    <p class="text-white text-opacity-50">Address: 54 Brisbane City QLD 4000</p>
+                    <p class="text-white text-opacity-50">Phone: 0404243454</p>
+                    <div class="mt-3">
+                        <h4>Follow Us</h4>
+                        <a href="https://www.facebook.com" class="text-white text-opacity-50 mx-2">
+                            <i class="bi bi-facebook" style="font-size: 1.5rem;"></i>
+                        </a>
+                        <a href="https://www.youtube.com" class="text-white text-opacity-50 mx-2">
+                            <i class="bi bi-youtube" style="font-size: 1.5rem;"></i>
+                        </a>
+                        <a href="https://www.twitter.com" class="text-white text-opacity-50 mx-2">
+                            <i class="bi bi-twitter" style="font-size: 1.5rem;"></i>
+                        </a>
+                        <a href="https://www.instagram.com" class="text-white text-opacity-50 mx-2">
+                            <i class="bi bi-instagram" style="font-size: 1.5rem;"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+       
+        <div class=" bg-dark p-3">
+            <p class="mb-0 text-center">&copy; 2024 Coding Diary. All rights reserved.</p>
+        </div>
+    </div>
+  
+
+</footer> 
