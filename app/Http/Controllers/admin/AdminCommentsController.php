16de763d31c49446c96aeaa73e64ea16b1a6e2b2
Dauth Gurung <dauth22grg22@gmail.com>
2024-09-10 22:36:22 +1000

    comments added

--- app/Http/Controllers/admin/AdminCommentsController.php
+++ app/Http/Controllers/admin/AdminCommentsController.php
@@ -13,24 +13,26 @@ class AdminCommentsController extends Controller
 {
     public function comments(Request $request){
         $search_comment = $request->input('search_comment');
-        $post_title_id = $request->input('post_title');
+        $post_title_id = $request->input('post_title'); 
 
         $query = Comment::with('replies')->with('blogPost')->orderBy('created_at','DESC');
         $posts = Post::orderBy('title', 'asc')->get();
 
+        //if there is search and post title selected
         if($search_comment && $post_title_id){
             $query->where('content', 'like', '%' . $search_comment . '%')
                  ->whereHas('blogPost',function ($query) use ($post_title_id){
                 $query->where('id',$post_title_id);
-                 });
-                    
+                 });         
         }
-        
+    
+        // if there is comment search only
         if($search_comment ){
             $query->where('content', 'like', '%' . $search_comment . '%');
                     
         }
 
+        // if post title is selected
         if($post_title_id){
             $query->whereHas('blogPost',function ($query) use ($post_title_id){
                 $query->where('id',$post_title_id);
@@ -42,10 +44,12 @@ class AdminCommentsController extends Controller
         $totalComments = $comments->count();
         return view('admin.comments',compact('comments','totalComments','search_comment','posts','post_title_id'));
     }
+    // comments method ends
 
     public function editComments(Comment $comment){
         return view('admin.edit_comments',compact('comment'));
     }
+    // editComments method ends
 
     public function updateComments(Request $request, Comment $comment){
         $validator = Validator::make($request->all(),[
@@ -63,16 +67,17 @@ class AdminCommentsController extends Controller
         return redirect()->route('admin.comments')->with('success', 'Comments has been successfully updated.');
         
     }
+    // updateComments ends
 
     public function destroy(Comment $comment){
         $comment->delete();
 
         return redirect()->back()->with('success','The comment has been successfully deleted.');
     }
+    //destroy method ends
 
     public function reply(Request $request,$comment_id){
         
-
         $validator = Validator::make($request->all(),[
             'comment_reply' =>'required|min:3',
         ]);
@@ -94,4 +99,5 @@ class AdminCommentsController extends Controller
 
         return redirect()->back()->with('success','You have replied to the comment successfully.');
     }
+    // reply method ends
 }
